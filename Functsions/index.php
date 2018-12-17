c8335fa2a541b40e12d1df4953153a67ef11038a
merle.pakko <merle.pakko@khk.ee>
2018-12-17 13:21:35 +0200

    Muutujad

--- Functsions/index.php
+++ Functsions/index.php
@@ -4,4 +4,20 @@
  * User: merle.pakko
  * Date: 17.12.2018
  * Time: 13:19
- */
+ */// operaatorid
+$arv1 = 7;
+$arv2 = 24;
+echo '<h4>Matemaatilised operaatorid</h4>';
+echo $arv1.' + '.$arv2.' = '.($arv1 + $arv2).'<br>';
+echo $arv1.' - '.$arv2.' = '.($arv1 - $arv2).'<br>';
+echo $arv1.' * '.$arv2.' = '.($arv1 * $arv2).'<br>';
+echo $arv1.' / '.$arv2.' = '.($arv1 / $arv2).'<br>';
+echo $arv1.' % '.$arv2.' = '.($arv1 % $arv2).'<br>';
+echo '<hr>';
+echo '<h4>Võrdlusoperaatorid</h4>';
+echo $arv1.' < '.$arv2.' see on '.($arv1 < $arv2).'<br>';
+echo $arv1.' > '.$arv2.' see on '.($arv1 > $arv2).'<br>';
+echo $arv1.' <= '.$arv2.' see on '.($arv1 <= $arv2).'<br>';
+echo $arv1.' >= '.$arv2.' see on '.($arv1 >= $arv2).'<br>';
+echo $arv1.' == '.$arv2.' see on '.($arv1 == $arv2).'<br>';
+echo $arv1.' != '.$arv2.' see on '.($arv1 != $arv2).'<br>';
