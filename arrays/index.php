c0af584b41b596d4b1b45263adfd68f0979b15b6
merle.pakko <merle.pakko@khk.ee>
2018-12-18 12:39:00 +0200

    arrayd

--- arrays/index.php
+++ arrays/index.php
@@ -1,25 +1,23 @@
 <?php
-$kasutajad = array (); //tühi massiiv
-$kasutajad[] = 'alice';
-$kasutajad[] = 'bob';
-array_push($kasutajad, var:'lucy');
-
-$lisakasutajad = array('mark','george');//andmetega massiiv
-$kasutajadkokku = count($kasutajad);
-for ($i = 0; $i < 3; $i++){
-    echo $kasutajad[$i]. '<br>';
-}
-array_shift($kasutajad);
-$kasutajadkokku = count($kasutajad);
-for ($i = 0; $i < 3; $i++) {
-    echo $kasutajad[$i] . '<br>';
-}
-
-$uuedKasutajad = array_merge($kasutajad, $lisakasutajad);
-for($i = 0; $i < count($uuedKasutajad); $i++) {
-    echo $uuedKasutajad[$i]. '<br>';
-}
-echo '<hr';
-foreach ($uuedKasutajad as $kasutaja) {
-    echo $kasutajad.'<br>';
+$kasutajad = array (
+    array (
+        'alice',
+        'Alice',
+        'Liddell',
+        'female'
+    ),
+    array(
+        'bob',
+        'Bob',
+        'Builder',
+        'male'
+    )
+);
+for($i = 0; $i < count($kasutajad); $i++){
+    if ($kasutajad[$i][3] == 'female')
+        echo '<div style= "color: red">';
+    }else {
+    echo '<div style = "color: blue">';
+    }
+    echo $info.'<div>';
 }
