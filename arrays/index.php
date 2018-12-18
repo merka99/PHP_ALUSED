141421165dcc94faa7e8d1f90ac01a3bc2d836c5
merle.pakko <merle.pakko@khk.ee>
2018-12-18 12:21:14 +0200

    arrayd

--- arrays/index.php
+++ arrays/index.php
@@ -2,11 +2,24 @@
 $kasutajad = array (); //tühi massiiv
 $kasutajad[] = 'alice';
 $kasutajad[] = 'bob';
-array_push($kasutajad, var'lucy');
+array_push($kasutajad, var:'lucy');
 
 $lisakasutajad = array('mark','george');//andmetega massiiv
-echo '<pre>';
-print_r($kasutajad);
-print_r($lisakasutajad);
-echo '</pre>';
-var_dump($kasutajad);
+$kasutajadkokku = count($kasutajad);
+for ($i = 0; $i < 3; $i++){
+    echo $kasutajad[$i]. '<br>';
+}
+array_shift($kasutajad);
+$kasutajadkokku = count($kasutajad);
+for ($i = 0; $i < 3; $i++) {
+    echo $kasutajad[$i] . '<br>';
+}
+
+$uuedKasutajad = array_merge($kasutajad, $lisakasutajad);
+for($i = 0; $i < count($uuedKasutajad); $i++) {
+    echo $uuedKasutajad[$i]. '<br>';
+}
+echo '<hr';
+foreach ($uuedKasutajad as $kasutaja) {
+    echo $kasutajad.'<br>';
+}
