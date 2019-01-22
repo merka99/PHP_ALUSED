fd7ae445f23376513c5d4687d58c022353699876
merle.pakko <merle.pakko@khk.ee>
2019-01-22 11:31:58 +0200

    Lisati erinevad massiivi stiilid

--- arrays/index.php
+++ arrays/index.php
@@ -32,3 +32,22 @@ $character ['special power'] = "x-ray vision";
 
 
 echo $character['age'];
+echo '<hr>';
+
+$users = array ("Bert", "Sharon", "Betty", "harry");
+echo $users[count($users) - 1];
+
+echo '<hr>';
+
+$users = array ("Bert", "Sharon", "Betty", "harry");
+foreach ($users as $val) {
+    print "$val<br>";
+}
+echo '<hr>';
+
+$character = array ('name'=>"Bert",
+    'occupation'=>"mida");
+foreach ($character as $key=>$val )
+{
+    print "$key = $val<br>";
+}
