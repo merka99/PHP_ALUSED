a84f4ac277f70577c21894951c75413878bb1cff
merle.pakko <merle.pakko@khk.ee>
2019-01-28 14:41:52 +0200

    uus kaust session koos vajalike failidega

---
+++ session/db/conf.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: merle.pakko
+ * Date: 28.01.2019
+ * Time: 14:20
+ */
--- session/index.php
+++ session/index.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
 require_once 'fnk.php';
-$loginForm = htmlFromFile('login');
-echo $loginForm;
+if(isset($_SESSION['user'])){
+    echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+    echo '<a href="acts/logout.php">Logi välja</a>';
+} else {
+    $loginForm = htmlFromFile('login');
+    echo $loginForm;
+}
