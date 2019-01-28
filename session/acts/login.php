53f62f45484d2e4387c1b4a8898b1793a693d1a0
merle.pakko <merle.pakko@khk.ee>
2019-01-28 13:59:11 +0200

    uus kaust session koos vajalike failidega

---
+++ session/acts/login.php
@@ -0,0 +1,5 @@
+<?php
+// andmed HTML vormi poolt
+$username = $_GET['uname'];
+$password = $_GET['upass'];
+echo $username.' '.$password;
---
+++ session/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'fnk.php';
+$loginForm = htmlFromFile('login');
+echo $loginForm;
