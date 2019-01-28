3f698a7319f5b82dae48b855cfbafb677e860014
merle.pakko <merle.pakko@khk.ee>
2019-01-28 13:27:21 +0200

    kuvatakse andmebaasi tabelis olevate kasutajate andmed

--- db/db_fnk.php
+++ db/db_fnk.php
@@ -1,12 +1,4 @@
 <?php
-require_once 'conf.php';
-require_once 'db_fnk.php';
-
-$iktConn = connect_db(DBHOST,DBUSER,DBPASS,DBNAME);
-
-echo '<pre>';
-print_r($iktConn);
-echo '</pre>';
 function connect_db($hostName, $dbUser, $dbPass, $dbName){
     $conn = mysqli_connect($hostName, $dbUser, $dbPass, $dbName);
     if(!$conn){
