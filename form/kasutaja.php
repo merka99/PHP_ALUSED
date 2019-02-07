a8d9d8e2bcde8df6c6a0a4a097fbc1082045b6d9
merka99 <merle.pakko@khk.ee>
2019-02-07 15:22:08 +0200

    php fail ja vorm

--- form/kasutaja.php
+++ form/kasutaja.php
@@ -1,9 +1,10 @@
-<?php
 <h1>Andmed </h1>
+<?php
 
 $tulemus = $_GET['nimi'];
-
+$date = = DateTime::createFromFormat('U.u', microtime(TRUE));
 
 echo 'Sinu nimi väikeste tähtedega: ';
 echo strtolower($tulemus);
+echo $date($date -> format('Y-m-d H:i:s.u'));
 ?>
